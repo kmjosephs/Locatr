acf265ebd72d4060c54b4ccd409cdda8ce79e55b
Keisha Josephs <kmjosephs@gmail.com>
2016-03-05 16:43:29 -0600

    Fixed problem with star rating

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -2,36 +2,34 @@
  * Created by kmjosephs on 3/2/16.
  */
 /* GET 'home' page */
-
-module.exports.homelist = function(req, res){
+module.exports.homelist = function(req, res) {
     res.render('locations-list', {
         title: 'Locatr - find a place to work with wifi',
         pageHeader: {
             title: 'Locatr',
             strapline: 'Find places to work with wifi near you!'
         },
-        sidebar: "Looking for wifi and a seat? Locatr helps you find places to work when out and about. " +
-        "Perhaps with coffee, cake or a pint? Let Locatr help you find the place you're looking for.",
-        locations:   [{
+        sidebar: "Looking for wifi and a seat? Locatr helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Locatr help you find the place you're looking for.",
+        locations: [{
             name: 'Starcups',
-            address: '125 High Street, Reading RG6 1PS',
+            address: '125 High Street, Reading, RG6 1PS',
             rating: 3,
             facilities: ['Hot drinks', 'Food', 'Premium wifi'],
             distance: '100m'
         }, {
             name: 'Cafe Hero',
-            address: '125 High Street, Reading RG6 1PS',
+            address: '125 High Street, Reading, RG6 1PS',
             rating: 4,
             facilities: ['Hot drinks', 'Food', 'Premium wifi'],
             distance: '200m'
         }, {
             name: 'Burger Queen',
-            address: '125 High Street, Reading RG6 1PS',
+            address: '125 High Street, Reading, RG6 1PS',
             rating: 2,
-            facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+            facilities: ['Food', 'Premium wifi'],
             distance: '250m'
         }]
-    }); // END res.render
+    });
 };
 
 /* GET 'Location info' page */
